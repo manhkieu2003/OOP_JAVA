c80b7041670e308c09abad02e2358c7166e48462
manhkieu2003 <139309650+manhkieu2003@users.noreply.github.com>
2024-09-16 20:21:09 +0700

    update oop

---
+++ src/main/java/org/example/demo/main.java
@@ -0,0 +1,8 @@
+package org.example.demo;
+
+public class main {
+    public static void main(String[] args) {
+        System.out.println("người anh em sà");
+        System.out.println("có lẽ");
+    }
+}
